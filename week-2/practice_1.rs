53e090829a55a9a08827488977764360c96be92a
Josemaria-1234 <josemaria.alozie@pau.edu.ng>
2025-10-06 23:01:30 +0100

    (My week 2 practices)

---
+++ week-2/practice_1.rs
@@ -0,0 +1,15 @@
+fn main(){
+	println!("Welcome to COS101!");
+
+	println!("The course learning outcome is at follows:");
+
+	println!("1. Distinguish between computer and computer programming,/n
+			  2. Develop some techniques in computer science,/n
+			  3. Understand the different areas of studying computer science,/n
+			  4. Conversant with applications of computer science./n
+			  5. Navigate the career prosprects in computer science./n
+			  6. Conversant with computer programming concepts.");
+	
+	
+
+}
