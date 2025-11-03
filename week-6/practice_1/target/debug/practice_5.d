f8b3feb31a18ab4f1ea69a1d6def8fef1dcf71a5
Josemaria-1234 <josemaria.alozie@pau.edu.ng>
2025-11-03 17:08:00 +0100

    (My first commit for week 6)

---
+++ week-6/practice_1/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\LENOVO\ T14\j.alozieCOS101\week-6\practice_1\target\debug\practice_5.exe: C:\Users\LENOVO\ T14\j.alozieCOS101\week-6\practice_1\src\main.rs
--- week-6/practice_5/target/debug/practice_5.d
+++
@@ -1,1 +0,0 @@
-C:\Users\LENOVO\ T14\j.alozieCOS101\week-6\practice_5\target\debug\practice_5.exe: C:\Users\LENOVO\ T14\j.alozieCOS101\week-6\practice_5\src\main.rs
