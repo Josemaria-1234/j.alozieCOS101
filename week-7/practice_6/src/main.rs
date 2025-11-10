46b4bc4907274dd25e5507cbd8b42f60e92cbacd
Josemaria-1234 <josemaria.alozie@pau.edu.ng>
2025-11-10 19:34:57 +0100

    (My sixth practice for week 7)

--- week-7/practice_6/src/main.rs
+++ week-7/practice_6/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("The sum of num is:{}",num);
 }
 
-fn mutate_num_to_zero(param_num:mut i32){
+fn mutate_num_to_zero(param_num:&mut i32){
     *param_num = *param_num*0;// de reference
     println!("The param_num value is :{}",param_num);
 }
