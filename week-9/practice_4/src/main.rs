f527de15eb5a59e32c2b3e5e9f3cfbefa6b3320e
Josemaria-1234 <josemaria.alozie@pau.edu.ng>
2025-11-24 09:51:06 +0100

    (My fourth practice for week 9)

--- week-9/practice_4/src/main.rs
+++ week-9/practice_4/src/main.rs
@@ -1,8 +1,8 @@
-use std::fs::Open options;
+use std::fs::OpenOptions;
 use std::io::Write;
 
 fn main() {
-    let mut fie = OpenOptions::new().append(true).open("data.txt").expect(
+    let mut file = OpenOptions::new().append(true).open("data.txt").expect(
         "cannot open file");
     file.write_all("\nHello Class".as_bytes()).expect("write failed");
     file.write_all("\nThis is the appendage to the document."
